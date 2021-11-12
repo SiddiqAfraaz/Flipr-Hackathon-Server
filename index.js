b0ed917383cfd571de68a6ff41d0a2d2f3dfbf43
Siddiq Afraaz <siddiqafraaz@gmail.com>
2021-11-12 20:25:15 +0530

    Mongo Cluster Setup

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import homeRoutes from './routes/home.js'
+
+const app = express();
+
+app.use('/', homeRoutes);
+
+app.use(express.json({limit: "30mb", extended: true}));
+app.use(express.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://SiddiqAfraaz:Siddiq123@flipr-hackathon.5bvji.mongodb.net/Flipr-Hackathon?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, ()=> console.log(`Server Started at Port: ${PORT}`)))
+    .catch((error)=>console.error(error));
+
