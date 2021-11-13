266fedb0393bcc04c68611e62ecbc55ea41b0e6b
Siddiq Afraaz <siddiqafraaz@gmail.com>
2021-11-13 10:48:13 +0530

    Add ENV

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,11 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.send(req.body)
+    res.send("Home Works")
+});
+
+router.get("/search", (req, res) => {
+    res.send("Search works")
 });
 
 export default router;
